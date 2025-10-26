fcc3ab161aeb32354b0109f6b536a0c2f5d96920
Mrinank-Bhowmick <77621953+Mrinank-Bhowmick@users.noreply.github.com>
2025-10-26 18:15:10 +0530

    add analytics

--- verbalize-nextjs/src/app/(dashboard)/layout.tsx
+++ verbalize-nextjs/src/app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import Sidebar from "@/components/sidebar";
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Verbalize | Dashboard",
@@ -20,6 +21,7 @@ export default function DashboardLayout({
         <div className="md:ml-68 ml-20 mt-2 mr-2 bg-gray-100 p-2 rounded-xl min-h-screen">
           {children}
           <SpeedInsights />
+          <Analytics />
         </div>
       </div>
     </ClerkProvider>
